2538329c7035e043a2a47757d14d698533cc93e5
伊東和成 <itokazunari5375@icloud.com>
2021-12-30 17:21:28 +0900

    v-showを定義

--- src/components/study/DirectivePage.vue
+++ src/components/study/DirectivePage.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="directive-page">
     <h2>学習2：ディレクティブ</h2>
+    <input type="checkbox" v-model="checked" />
+    <span v-show="checked">チェックの時だけ見えるよ</span>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      checked: true,
+    };
+  },
+};
 </script>
 
 <style scoped>
