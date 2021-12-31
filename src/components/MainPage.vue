d7bab5101b1d5cc83db7d192ec820206b6520a2d
伊東和成 <itokazunari5375@icloud.com>
2021-12-31 11:49:50 +0900

    新規ノートの作成

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="main-page">
-    <div class="left-menu">左メニュー</div>
+    <div class="left-menu">
+      <!-- ノートリスト -->
+      <NoteItem
+        v-for="note in noteList"
+        v-bind:key="note.id"
+        v-bind:note="note"
+      />
+      <!-- ノートの追加ボタン -->
+      <button class="transparent" @click="onClickButtonAdd">
+        <i class="fas fa-plus-square"></i>ノートを追加
+      </button>
+    </div>
     <div class="right-view">右ビュー</div>
   </div>
 </template>
 
 <script>
-export default {};
+import NoteItem from "@/components/parts/NoteItem.vue";
+export default {
+  data() {
+    return {
+      noteList: [],
+    };
+  },
+  methods: {
+    onClickButtonAdd: function () {
+      this.noteList.push({
+        id: new Date().getTime().toString(16),
+        name: `新規ノート`,
+      });
+    },
+  },
+  components: {
+    NoteItem,
+  },
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+button.transparent {
+  margin: 5px;
+  background: transparent;
+  border: none;
+}
 .main-page {
   display: flex;
   height: calc(100vh - 60px);
